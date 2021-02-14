19fcff4d73f7136ece429e817489ff4c1dd8761b
Andriy Palyukha <apalyukha@gmail.com>
2021-02-14 13:08:49 +0200

    hiding pass and secret keys in env

--- worldwomen/worldwomen/settings.py
+++ worldwomen/worldwomen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-mduicjv7a7#=q20##mc$q%(jn0fb-+z51@t(2wh%twh13c-gq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
